c4a718f93ec33f6aed2ed33ab66867bb8a9b3337
Zoltan Szilvasy <szilvasyz@gmail.com>
2023-11-17 08:38:39 +0100

    beginning playfiles example

--- examples/playfiles/playfiles.ino
+++ examples/playfiles/playfiles.ino
@@ -1,9 +1,13 @@
-#define USE_SDFATLIB
-//#define USE_SDLIB
-
 #include "AVRPCM.h"
 #include "mybutton.h"
 #include "WAVhdr.h"
+#include "SdFat.h"
+
+#define SD_CS 4
+#define SD_FAT_TYPE 0
+#define SPI_CLOCK SD_SCK_MHZ(20)
+#define SD_CONFIG SdSpiConfig(SD_CS, DEDICATED_SPI, SPI_CLOCK)
+
 
 #if defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
 #define DIG_OUTPIN  6
@@ -14,89 +18,25 @@
 #define BT1_PIN 18
 #define BT2_PIN 19
 
-#define SDCS 4
 
 #define INIT_TRY 5
 
-#define FILENAME "chaos22.wav"
-
-//MyButton btn({1,2});
-MyButton btn;
-
-#ifdef USE_SDLIB
-#include <SPI.h>
-#include <SD.h>
-#define SDINIT SD.begin(SDCS)
-#define DATAFILEOPEN dataFile = SD.open(FILENAME)
-#endif
-
-#ifdef USE_SDFATLIB
-#include "SdFat.h"
-#define SD_FAT_TYPE 0
-#define SPI_CLOCK SD_SCK_MHZ(20)
-#define SD_CONFIG SdSpiConfig(SDCS, DEDICATED_SPI, SPI_CLOCK)
-#define SDINIT sd.begin(SD_CONFIG)
-#define DATAFILEOPEN dataFile.open(FILENAME, O_RDONLY)
+MyButton btn(BT1_PIN, BT2_PIN);
 SdFat sd;
-#endif
-
 File dataFile;
 WAVhdr W;
+
 
 int sdready = false;
 
 
-void setup() {
-  int init_cnt = INIT_TRY;
-
-  btn.add(BT1_PIN);
-  btn.add(BT2_PIN);
-  btn.begin();
-
-  pinMode(SDCS, OUTPUT);
-  digitalWrite(SDCS, HIGH);
-  
-  Serial.begin(115200);
-  Serial.println("Start");
-
-  delay(1000);
-  
-  while (!(sdready = SDINIT) && init_cnt--) {
-    Serial.print("#");
-    digitalWrite(SDCS, HIGH);
-    delay(500);
-  }
-
-  if (!sdready) {
-    Serial.println("Card failed, or not present");
-  }
-  else {
-    Serial.println("card initialized.");
-  }
-
-  Serial.print("Init: ");
-  Serial.println(PCM_init(DIG_OUTPIN));
-  Serial.print("Setup: ");
-  Serial.println(PCM_setupPWM(16000, 0));
-  Serial.print("StartGen: ");
-  Serial.println(PCM_startGen(1000, 80, PCM_SQUARE));
-  delay(2000);
-  Serial.print("Stop: ");
-  Serial.println(PCM_stop());
-  Serial.print("Card present: ");
-  Serial.println(sdready);
-}
-
-void loop() {
-  int b;
+void playfile() {
   uint16_t sr = 48000;
   uint32_t ds = 0, ss = 0;
   int pct;
   int pct0 = 0;
-  
-  while (btn.get() == 0);
 
-  DATAFILEOPEN;
+  dataFile.open("chaos.wav", O_RDONLY);
 
   if (dataFile.available()) {
     Serial.println();
@@ -147,5 +87,46 @@ void loop() {
 
   Serial.print("Stop play: ");
   Serial.println(PCM_stop());
+}
 
+void setup() {
+  int init_cnt = INIT_TRY;
+
+  Serial.begin(115200);
+  Serial.println("Start");
+
+  btn.begin();
+
+  pinMode(SD_CS, OUTPUT);
+  digitalWrite(SD_CS, HIGH);
+  
+  delay(1000);
+  
+  while (!(sdready = sd.begin(SD_CONFIG)) && init_cnt--) {
+    Serial.print("#");
+    digitalWrite(SD_CS, HIGH);
+    delay(500);
+  }
+
+  if (!sdready) {
+    Serial.println("Card failed, or not present");
+  }
+  else {
+    Serial.println("card initialized.");
+  }
+
+  Serial.print("Init: ");
+  Serial.println(PCM_init(DIG_OUTPIN));
+  Serial.print("Buttons: ");
+  Serial.println(btn.count());
+  
+}
+
+void loop() {
+  int b;
+  
+  while (btn.peek() == 0);
+  Serial.print(btn.get());
+  Serial.print(' ');
+  
 }
